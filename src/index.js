c824a5d06a6b2e3022bcc44f16aed6895988d714
GitWorkingTime <dlee28262@gmail.com>
2025-12-30 00:21:08 -0500

    Planned a layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
+// Entry Point script
 import "./styles.css";
+
+/*
+ * To-Do list setup:
+ * 
+ * Inspiration: Trello
+ * - Project Lists:
+ *  - Category of todo list tasks
+ *  - Can...
+ *      - Edit title
+ *      - Delete list
+ *      - Add list
+ *      - Add todo list items
+ *      - Delete todo list items
+ * 
+ * - To-do list items:
+ *  - Properties
+ *      - Title
+ *      - Description
+ *      - Deadline
+ *      - Checkmark complete
+ *      - Includes a checklist
+ *          - Checkbox
+ *          - Items
+ *          - Edit title and tasks
+ *          - Progress bar
+ *          - Indicator of how much is completed
+ * 
+ * Functions:
+ *  - Create list
+ *  - Edit list details
+ *      - Edit title
+ *  - Delete list
+ *  - Add item
+ *  - Delete item
+ *  - Edit item details
+ *      - Edit title
+ *      - Edit description
+ *      - Edit deadline
+ *      - Edit checklist
+ *      - Edit completed
+ * 
+ */
+
+
